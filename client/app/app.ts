cab5c05cd2fa8456191832c943c23eaff3600b3a
Gabor Pankotay <mrpanky@gmail.com>
2015-11-20 09:26:54 +0100

    starmap

--- client/app/app.ts
+++ client/app/app.ts
@@ -2,13 +2,14 @@
 
 import {Component, View, NgFor, bootstrap} from "angular2/angular2";
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
+import Starmap from "../starmap/starmap";
 
 @Component({
     selector: "app"
 })
 @View({
     templateUrl: "client/app/app.html",
-    directives: [NgFor, ROUTER_DIRECTIVES]
+    directives: [NgFor, ROUTER_DIRECTIVES, Starmap]
 })
 class App {
     constructor() {
--- server/main.ts
+++ server/main.ts
@@ -1,13 +1,12 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
-import GalaxyGenerator from "./install/galaxyGenerator";
+import {createSpiralGalaxy} from "./install/galaxyGenerator";
 
 Meteor.startup(function () {
     console.log("Server started...");
     let initialized = false;
 
     if (!initialized) {
-        let generator = new GalaxyGenerator();
-        let planets = generator.createSpiralGalaxy(
+        let planets = createSpiralGalaxy(
             800, //width
             800, //height
             3000, //number

803b1bac1990999ab80adb241c3cb95e47195d89
Gabor Pankotay <mrpanky@gmail.com>
2015-11-20 09:12:29 +0100

    basic galaxy conf

---
+++ lib/config.ts
@@ -0,0 +1,12 @@
+export default class Config {
+    galaxy = {
+        x: {
+            min: -2000,
+            max: 2000
+        },
+        y: {
+            min: -2000,
+            max: 2000
+        }
+    }
+}
